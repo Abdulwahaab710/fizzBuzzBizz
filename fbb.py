ace9e565e3ca07073f8c745d3b919c029a31e8d2
Ashwin Panchapakesan <inspectorG4dget@users.noreply.github.com>
2017-03-25 18:16:05 -0400

    Update fbb.py

--- fbb.py
+++ fbb.py
@@ -1,16 +1,9 @@
 def fbb():
     for num in range(1,101):
-        if all(num%i == 0 for i in (3,5,7)):
-            print("FizzBuzzBiz")
-        elif all(num%i == 0 for i in (3,5)):
-            print("FizzBuzz")
-        elif all(num%i == 0 for i in (5,7)):
-            print("BuzzBizz")
-        elif not num%3:
-            print("Fizz")
-        elif not num%5:
-            print("Buzz")
-        elif not num%7:
-            print("Bizz")
-        else:
-            print(num)
+        out = []
+        if not num%3: out.append("Fizz")
+        if not num%5: out.append("Buzz")
+        if not num%7: out.append("Bizz")
+
+        if not out: print(num)
+        else: print(''.join(out))
